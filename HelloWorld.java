8186a1002e04892200b4de8d5088d5578ed76a8e
sathvik-s10 <sathvik.s@outlook.com>
2026-09-20 01:07:20 -0700

    test
    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
-package TestProject;
+
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Bye World!");
 
     }
 
